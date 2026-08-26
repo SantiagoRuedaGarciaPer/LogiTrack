f6f481b45b417f1d7bb58397e14402f5f0ed980e
Santiago Rueda Garcia <santirueda0917@gmail.com>
2026-08-26 06:58:55 -0500

    :feat: EmpleadoMapper implemented

---
+++ src/main/java/Mapper/EmpleadoMapper.java
@@ -0,0 +1,47 @@
+package Mapper;
+
+import DTO.Request.EmpleadoRequest;
+import DTO.Response.EmpleadoResponse;
+import Model.Empleado;
+
+public class EmpleadoMapper {
+    public EmpleadoResponse entityToDto(Empleado empleado){
+        if (empleado == null) return null;
+
+        return new EmpleadoResponse(
+                empleado.getId(),
+                empleado.getNombre(),
+                empleado.getEmail(),
+                empleado.getTipoDocumento(),
+                empleado.getDocumento(),
+                empleado.getCargo()
+        );
+
+    }
+
+    public Empleado DtoToEntity(EmpleadoRequest dto){
+        if(dto == null) return null;
+
+        Empleado e = new Empleado();
+        e.setNombre(dto.nombre());
+        e.setEmail(dto.email());
+        e.setTipoDocumento(dto.tipoDocumento());
+        e.setDocumento(dto.documento());
+        e.setCargo(dto.cargo());
+
+        return e;
+    }
+
+    public void UpdateDtoToEntity(Empleado empleado, EmpleadoRequest dto){
+        if(empleado == null || dto == null) return;
+
+        empleado.setNombre(dto.nombre());
+        empleado.setEmail(dto.email());
+        empleado.setTipoDocumento(dto.tipoDocumento());
+        empleado.setDocumento(dto.documento());
+        empleado.setCargo(dto.cargo());
+
+    }
+
+
+}
